5347837e4f0ce82f980a7978496c62baf792f2a2
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-03-10 17:58:45 +0200

    much docs!

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,25 +1,29 @@
-// not a generated file!
 package handlers
 
 import (
-	"github.com/baraa-almasri/useless/songs"
 	"net/http"
 )
 
+// AddURL adds a URL to the short urls list and returns the assigned short url
 func AddURL(w http.ResponseWriter, r *http.Request) {
 	url := r.URL.Query()["url"][0]
-	createAndUpdate(url)
+	short := createAndUpdate(url)
+	w.Write([]byte("http://shorts.ninja/" + short))
 }
 
+// GetURL redirects to the full URL from the given shortURL
+// if no url is assigned it plays a meme song :)
 func GetURL(w http.ResponseWriter, r *http.Request) {
 	url := getFullURL(r.URL.Path)
 	http.Redirect(w, r, url, http.StatusFound)
 }
 
+// PlayMemeSong plays a random meme song, wow!
 func PlayMemeSong(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, songs.NewMemeSongs().GetRandomSong(), http.StatusFound)
+	http.Redirect(w, r, memes.GetRandomSong(), http.StatusFound)
 }
 
+// RickRoll redirects to Rick Astley's - Never Gonna Give You Up YT Video, perfect RickRolling :)
 func RickRoll(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "https://www.youtube.com/watch?v=dQw4w9WgXcQ", http.StatusFound)
 }
--- handlers/utils.go
+++ handlers/utils.go
@@ -2,10 +2,16 @@ package handlers
 
 import (
 	"github.com/baraa-almasri/shortsninja/globals"
+	"github.com/baraa-almasri/useless/songs"
 	"io/ioutil"
 	"os"
 )
 
+var (
+	memes = songs.NewMemeSongs()
+)
+
+// getFullURL retrieves a URL from its corresponding shortURL file
 func getFullURL(shortURL string) string {
 	url, err := ioutil.ReadFile("./urls/" + shortURL)
 	if err != nil {
@@ -14,6 +20,7 @@ func getFullURL(shortURL string) string {
 	return string(url)
 }
 
+// elementExistInArr returns true if the given element exists in the given slice
 func elementExistInArr(element string, slice []string) bool {
 	for i := range slice {
 		if slice[i] == element {
@@ -24,7 +31,9 @@ func elementExistInArr(element string, slice []string) bool {
 	return false
 }
 
-func createAndUpdate(url string) {
+// createAndUpdate creates a new url file that doesn't exist in the usedURLs slice and updates the usedURLs slice
+// and returns the assigned short URL
+func createAndUpdate(url string) string {
 	for _, v := range globals.ShortURLs {
 		if !elementExistInArr(v, globals.UsedShortURLs) {
 			f, _ := os.Create("./urls/" + v)
@@ -32,7 +41,9 @@ func createAndUpdate(url string) {
 			_ = f.Close()
 
 			globals.UsedShortURLs = append(globals.UsedShortURLs, v)
-			return
+			return v
 		}
 	}
+
+	return ""
 }
