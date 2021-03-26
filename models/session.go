1ce5cca40b1b60c38b6097a30ee45ad6469264dc
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-03-26 15:27:31 +0300

    added session section!

--- models/session.go
+++ models/session.go
@@ -2,6 +2,7 @@ package models
 
 // Session defines session's properties(of a registered user)
 type Session struct {
-	Token string `json:"token"`
-	IP    string `json:"ip"`
+	Token     string
+	IP        string
+	UserEmail string
 }
